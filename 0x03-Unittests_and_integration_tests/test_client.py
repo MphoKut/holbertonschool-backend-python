57b728079a3120e8e964bce75fd9753db63a4e0b
Mpho Kutoane <mkutoane@singular.co.za>
2023-01-09 15:45:37 +0200

    Unit tests

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import unittest
+from unittest.mock import patch, Mock, PropertyMock
+from parameterized import parameterized
+
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_json):
+        endpoint = 'https://api.github.com/orgs/{}'.format(org_name)
+        spec = GithubOrgClient(data)
+        spec.org()
+        mock_json.assert_called_once_with(endpoint)
+
+    @parameterized.expand([
+        ("random-url", {'repos_url': 'http://some_url.com'})
+    ])
+    def test_public_repos_url(self, name, result):
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=result)):
+            response = GithubOrgClient(name)._public_repos_url
+            self.assertEqual(response, result.get('repos_url'))
